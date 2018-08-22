350f3a2324c493918e11493fe5f115f5119c1976
Adam Runner <adamrunner@gmail.com>
2018-08-22 12:46:27 -0700

    update production specific deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,6 +4,7 @@
 # You can define all roles on a single server, or split them:
 
 server "adamrunner.com", user: "adamrunner", roles: %w{app db web}
+set :branch, "production"
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
