9e34f8d8b2fed3bd8e6c7b00255b196979f2f485
Adam Runner <adamrunner@gmail.com>
2019-11-28 22:35:53 -0800

    WIP commit for temperature wrapper for unknown type of temperature data

--- app/fields/temperature.rb
+++ app/fields/temperature.rb
@@ -1,6 +1,10 @@
-class Temperature < Unit
-  def initialize(value, unit = "ºF")
-    @value = value.to_f
-    @unit  = unit
+class Temperature
+  def self.new(value, unit)
+    if unit.blank?
+    elsif unit.match(/F/)
+      TemperatureF.new(value, unit)
+    elsif unit.match(/C/)
+      TemperatureC.new(value, unit)
+    end
   end
 end
